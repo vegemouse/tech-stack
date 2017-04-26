93b415da36a5b684d9303a3d1a0a95ce129362e6
Maxwell Cady <maxwell.josef@gmail.com>
2017-04-26 14:02:31 -0700

    show animation on tap

---
+++ src/components/ListItem.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.easeInEaseOut();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
+  render() {
+    const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  // If they're the same, return true, otherwise return false
+
+  return { expanded }
+  // expanded: expanded
+};
+//ownProps is the same as this.props inside of component above
+
+export default connect(mapStateToProps, actions)(ListItem);
+//null if there is no mapStateToProps function
+//actions causes action creator to dispatch action to redux store. turns actions into props
